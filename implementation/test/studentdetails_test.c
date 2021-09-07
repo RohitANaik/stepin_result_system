8ff559d32ece9ebe3f962d6eba33c9559251bd00
RohitANaik <89580721+RohitANaik@users.noreply.github.com>
2021-09-07 12:44:01 +0530

    Add files via upload

---
+++ implementation/test/studentdetails_test.c
@@ -0,0 +1,60 @@
+#include "unity.h"
+#include "studentdetails.h"
+
+void setUp()
+{
+
+}
+
+void tearDown()
+{
+
+}
+
+void test_add_student(void)
+{
+  student_t new_student = {"Rohit","01feqwert",22,99};
+  TEST_ASSERT_EQUAL(0, add_student(&new_student));
+}
+
+
+void test_delete_student(void)
+{
+  student_t new_student = {"Rohit","01feqwert",22,99};
+  TEST_ASSERT_EQUAL(0, add_student(&new_student));
+  TEST_ASSERT_EQUAL(0, delete_student(new_student.name));
+}
+
+void test_modify_student(void)
+{
+  student_t new_student = {"Rohit","01feqwert",22,99};
+  student_t new_student1 = {"RohitA","051feqwert",24,96};
+  TEST_ASSERT_EQUAL(0, add_student(&new_student));
+  TEST_ASSERT_EQUAL(0, modify_student(new_student.name,&new_student1));
+  TEST_ASSERT_EQUAL(0, delete_student(new_student.name));
+}
+void test_search_student(void)
+{
+  student_t new_student = {"Rohit","01feqwert",22,99};
+  TEST_ASSERT_EQUAL(0, add_student(&new_student));
+  TEST_ASSERT_EQUAL(0, search_student(new_student.name));
+  TEST_ASSERT_EQUAL(0, delete_student(new_student.name));
+}
+
+void test_display_student(void)
+{
+  TEST_ASSERT_EQUAL(1, display_student()>0);
+}
+
+int main(void)
+{
+  UNITY_BEGIN();
+
+  RUN_TEST(test_add_student);
+  RUN_TEST(test_delete_student);
+  RUN_TEST(test_modify_student);
+  RUN_TEST(test_search_student);
+  RUN_TEST(test_display_student);
+
+  return UNITY_END();
+}
