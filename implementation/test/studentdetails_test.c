be5116ce2555bd69eeba16c1bd2c3845e66686bf
RohitANaik <89580721+RohitANaik@users.noreply.github.com>
2021-09-08 20:06:26 +0530

    Update studentdetails_test.c

--- implementation/test/studentdetails_test.c
+++ implementation/test/studentdetails_test.c
@@ -14,36 +14,36 @@ void tearDown()
 void test_add_student(void)
 {
   student_t new_student = {"abc","01feqwert",22,99};
-  TEST_ASSERT_EQUAL(1, add_student(&new_student));
+  TEST_ASSERT_EQUAL(0, add_student(&new_student));
 }
 
 
 void test_delete_student(void)
 {
   student_t new_student = {"abcd","01feqwerqt",223,939};
-  TEST_ASSERT_EQUAL(1, add_student(&new_student));
-  TEST_ASSERT_EQUAL(2, delete_student(new_student.name));
+  TEST_ASSERT_EQUAL(0, add_student(&new_student));
+  TEST_ASSERT_EQUAL(0, delete_student(new_student.name));
 }
 
 void test_modify_student(void)
 {
   student_t new_student = {"Rohit","01feqwert",22,99};
   student_t new_student1 = {"RohitA","051feqwert",24,96};
-  TEST_ASSERT_EQUAL(1, add_student(&new_student));
-  TEST_ASSERT_EQUAL(3, modify_student(new_student.name,&new_student1));
-  TEST_ASSERT_EQUAL(2, delete_student(new_student.name));
+  TEST_ASSERT_EQUAL(0, add_student(&new_student));
+  TEST_ASSERT_EQUAL(0, modify_student(new_student.name,&new_student1));
+  TEST_ASSERT_EQUAL(0, delete_student(new_student.name));
 }
 void test_search_student(void)
 {
   student_t new_student = {"Rohit","01feqwert",22,99};
-  TEST_ASSERT_EQUAL(1, add_student(&new_student));
-  TEST_ASSERT_EQUAL(4, search_student(new_student.name));
-  TEST_ASSERT_EQUAL(2, delete_student(new_student.name));
+  TEST_ASSERT_EQUAL(0, add_student(&new_student));
+  TEST_ASSERT_EQUAL(0, search_student(new_student.name));
+  TEST_ASSERT_EQUAL(0, delete_student(new_student.name));
 }
 
 void test_display_student(void)
 {
-  TEST_ASSERT_EQUAL(5, display_student()>0);
+  TEST_ASSERT_EQUAL(1, display_student()>0);
 }
 
 int main(void)
